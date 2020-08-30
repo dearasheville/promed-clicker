cb8d626a5de9c8aa70b629964701f4f7d8901edd
dearasheville <dearasheville@outlook.com>
2020-08-30 23:04:48 +0500

    paste implementation added

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,32 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 
+import robot from 'robotjs';
+import clipboardy from 'clipboardy';
+
 const sleep = (ms) => {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 };
 
-export default sleep;
+const toPaste = {
+  lower: (data) => {
+    clipboardy.writeSync(data);
+
+    robot.mouseClick();
+    robot.mouseClick('right');
+
+    robot.moveMouse(robot.getMousePos()['x'] + 100, robot.getMousePos()['y'] + 190);
+    robot.mouseClick();
+  },
+  upper: (data) => {
+    clipboardy.writeSync(data);
+
+    robot.mouseClick();
+    robot.mouseClick('right');
+
+    robot.moveMouseSmooth(robot.getMousePos()['x'] + 140, robot.getMousePos()['y'] - 245);
+    robot.mouseClick();
+  },
+};
+
+export { sleep, toPaste };
