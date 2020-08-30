2810d3bf5075205c7c077fc47bd4072823e718a5
dearasheville <dearasheville@outlook.com>
2020-08-31 02:22:50 +0500

    minor updates

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import clicker from './bin/medicalClicker';
 import spreadsheet from './bin/medicalSpreadsheet';
 
 const getData = (array) => {
-  const line = spreadsheet[0];
+  const line = array;
 
-  const person = array[0].split(' ');
+  const person = line[0].split(' ');
 
   const surname = person[0];
   const name = person[1];
   const pathronymic = person[2];
-  const birth = array[1];
+  const birth = line[1];
 
-  const department = array[2];
-  const clinician = array[3];
-  const diseaseCode = array[4];
+  const department = line[2];
+  const clinician = line[3];
+  const diseaseCode = line[4];
 
-  const visitCode = array[5];
-  const medicalService = array[6];
+  const visitCode = line[5];
+  const medicalService = line[6];
 
   return clicker(surname, name, pathronymic, birth, department, clinician, diseaseCode, visitCode, medicalService);
 };
