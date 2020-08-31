e4e9a9eadebcf353f231f3a229ea8961f2b25068
dearasheville <dearasheville@outlook.com>
2020-08-31 23:19:53 +0500

    displaying the window in a separate module

---
+++ src/clicker/tnm.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+
+import robot from 'robotjs';
+import { toClick, sleepUntilGetCorrectPixel } from '../utils';
+
+const tnm = () => {
+  sleepUntilGetCorrectPixel(719, 507, '36383c');
+
+  toClick.smooth(990, 635);
+
+  sleepUntilGetCorrectPixel(872, 247, 'ffffff');
+
+  toClick.smooth(1865, 1050);
+
+  sleepUntilGetCorrectPixel(878, 896, '4d5b6e');
+
+  toClick.smooth(130, 1050);
+};
+
+export default tnm;
