750652d40662889c1c3013ffa373503ee085df49
dearasheville <dearasheville@outlook.com>
2020-09-01 23:59:45 +0500

    minor updates

--- src/bin/test.js
+++ src/bin/test.js
@@ -2,10 +2,18 @@
 /* eslint-disable no-undef */
 
 import robot from 'robotjs';
+import clipboardy from 'clipboardy';
+
 import { toSleep, toPaste } from '../utils';
 
-toSleep(5000);
+// toSleep(5000);
 
-console.log(robot.getMousePos());
-console.log(robot.getPixelColor(robot.getMousePos().x, robot.getMousePos().y));
+// console.log(robot.getMousePos());
+// console.log(robot.getPixelColor(robot.getMousePos().x, robot.getMousePos().y));
 // console.log(robot.getPixelColor(103, 842));
+
+const test = clipboardy.readSync();
+
+
+console.log(test.indexOf('874737'));
+
