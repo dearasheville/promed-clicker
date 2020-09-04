f01b183f62615cb32dd23192aec26b7bed0e8b8a
dearasheville <dearasheville@outlook.com>
2020-09-05 02:36:07 +0500

    bug fixies

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ const getData = (array, first, second) => {
   const surname = person[0];
   const name = person[1];
   const pathronymic = person[2];
-  const birth = line[1];
+  const birth = line[2];
 
-  const department = line[3];
-  const clinician = line[4];
-  const diseaseCode = String(line[2]);
+  const diseaseCode = String(line[3]);
+  const department = line[4];
+  const clinician = line[5];
 
   const visitCode = String(first);
   const medicalService = String(`A${second.slice(1)}`);
@@ -24,9 +24,9 @@ const getData = (array, first, second) => {
   // const visitCode = String(line[5]);
   // const medicalService = String(`A${line[6].slice(1)}`);
 
-  const diagnost = String(line[6].split(' ').join(' '));
+  const diagnost = String(line[7].split(' ').join(' '));
 
-  const date = String(line[5]);
+  const date = String(line[6]);
   // console.log(surname, name, pathronymic, birth, department, clinician, diseaseCode, visitCode, medicalService, diagnost, date);
   return clicker(surname, name, pathronymic, birth, department, clinician, diseaseCode, visitCode, medicalService, diagnost, date);
 };
