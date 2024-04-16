4abfc2408b33652349cf9e95c0ff464b1366a6e4
dearasheville <dearasheville@outlook.com>
2024-04-16 21:37:37 +0500

    sequence of functions in paraclinical clicker added

--- src/clicker/paraclinic/clicker.js
+++ src/clicker/paraclinic/clicker.js
@@ -1,5 +1,21 @@
-const paraclinicClicker = (...data) => {
-  console.log(...data);
+import {
+  sleepForMs,
+} from '../../utils/sleep.js';
+
+import requests from './requests.js';
+import search from './search.js';
+import direction from './direction.js';
+import result from './result.js';
+
+const paraclinicClicker = (patient, clinician, diagnost, service) => {
+  sleepForMs(2500);
+
+  requests.stage1();
+  search(patient);
+  const number = direction(clinician, service);
+
+  requests.stage2(patient, number);
+  result(diagnost, service);
 };
 
 export default paraclinicClicker;
