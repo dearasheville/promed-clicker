e2dfa1d1888cbf12f7145c1344c09ab25736ef44
dearasheville <dearasheville@outlook.com>
2024-04-16 21:44:42 +0500

    added default values

--- src/input-devices/keyboard.js
+++ src/input-devices/keyboard.js
@@ -4,34 +4,59 @@ import clipboardy from 'clipboardy';
 const keyboardStandartDelay = 10;
 
 const keyboard = {
-  tap: (key, modifier, count = 1) => {
+  tap: (key = '', modifier = [], count = 1, delay = keyboardStandartDelay) => {
+    robot.setKeyboardDelay(delay);
+
+    const normalizedKey = String(key);
+
     let acc = count;
 
     while (acc > 0) {
-      robot.keyTap(key, modifier);
+      robot.keyTap(normalizedKey, modifier);
 
       acc -= 1;
     }
+
+    robot.setKeyboardDelay(keyboardStandartDelay);
   },
-  type: (text, delay = keyboardStandartDelay) => {
+  type: (text = '', delay = keyboardStandartDelay) => {
     robot.setKeyboardDelay(delay);
 
-    text.split('').forEach((letter) => robot.keyTap(letter));
+    const normalizaedText = String(text);
+
+    // normalizaedText.split('').forEach((letter) => robot.keyTap(letter));
+    normalizaedText.split('').forEach((letter) => keyboard.paste(letter));
 
     robot.setKeyboardDelay(keyboardStandartDelay);
   },
   copy: () => {
-    robot.keyTap('c', 'control');
+    keyboard.tap('c', ['control']);
 
-    return clipboardy.readSync();
+    const data = clipboardy.readSync();
+
+    return data;
   },
   paste: (data = '') => {
-    clipboardy.writeSync(data);
+    const normalizedData = String(data);
 
-    robot.keyTap('v', 'control');
+    clipboardy.writeSync(normalizedData);
+
+    keyboard.tap('v', ['control']);
   },
-  select: () => {
-    robot.keyTap('a', 'control');
+  selectAll: () => {
+    keyboard.tap('a', ['control']);
+  },
+  scroll: (key = 'top') => {
+    switch (key) {
+      case 'top':
+        keyboard.tap('pageup', [], 5);
+        break;
+      case 'bottom':
+        keyboard.tap('pagedown', [], 5);
+        break;
+      default:
+        break;
+    }
   },
 };
 
