4e87281690c5cb2ee56dfee431698f7cb0e3a944
dearasheville <dearasheville@outlook.com>
2024-04-16 21:49:45 +0500

    added a second separate function to wait for pixel color match/unmatch

--- src/utils/sleep.js
+++ src/utils/sleep.js
@@ -3,13 +3,19 @@ import doPointColorAndListColorMatch from './colors-match.js';
 const sleepForMs = (ms) => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 
 const sleepUntilPointColorMatchesList = (point, ...colors) => {
-  const doTheColorsMatch = doPointColorAndListColorMatch(point, ...colors);
+  while (doPointColorAndListColorMatch(point, colors)) {
+    sleepForMs(10);
+  }
+};
 
-  while (!doTheColorsMatch) {
+const sleepUntilPointColorUnmatchesList = (point, ...colors) => {
+  while (!doPointColorAndListColorMatch(point, colors)) {
     sleepForMs(10);
   }
 };
 
 export {
-  sleepForMs, sleepUntilPointColorMatchesList,
+  sleepForMs,
+  sleepUntilPointColorMatchesList,
+  sleepUntilPointColorUnmatchesList,
 };
