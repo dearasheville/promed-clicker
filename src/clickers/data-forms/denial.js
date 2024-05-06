a1d8e4b197bbda8ebd24fe0d03f230b8266cf392
dearasheville <dearasheville@outlook.com>
2024-05-06 16:43:25 +0500

    added select button and cause form data for denial

---
+++ src/clickers/data-forms/denial.js
@@ -0,0 +1,22 @@
+const selectButtonData = {
+  point: {
+    x: 757,
+    y: 691,
+  },
+  width: 75,
+  height: 21,
+};
+
+const causeFormData = {
+  point: {
+    x: 765,
+    y: 541,
+  },
+  width: 450,
+  height: 22,
+};
+
+export {
+  selectButtonData,
+  causeFormData,
+};
