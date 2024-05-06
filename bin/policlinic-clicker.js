4c6e6f00f1f00eba1b156778ade3d71bd471fab8
dearasheville <dearasheville@outlook.com>
2024-05-06 14:59:13 +0500

    rtmis policlinic module support abandoned

--- bin/policlinic-clicker.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import policlinicClicker from '../src/clicker/policlinic/clicker.js';
-
-import clickerEngine from '../src/index.js';
-
-clickerEngine(policlinicClicker);
--- bin/policlinic-remover.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import policlinicRemover from '../src/remover/policlinic/remover.js';
-
-import clickerEngine from '../src/index.js';
-
-clickerEngine(policlinicRemover);
